d8a8215db003400c2a1cacf321032e1bb3b447f2
15662650727 <447661866@qq.com>
2018-04-01 21:59:09 +0800

    1

---
+++ src/com/springJdbc/springJdbcTest.java
@@ -0,0 +1,23 @@
+package com.springJdbc;
+
+import com.spring.BeanA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class springJdbcTest {
+
+    @org.junit.Test
+    public void test(){
+        //测试数据库和SpringJdbc可以通过
+        ApplicationContext ap = new ClassPathXmlApplicationContext("com/springJdbc/dispatcher-servlet.xml");
+        JdbcTemplate jdbcTemplate = (JdbcTemplate) ap.getBean("jt");
+        List list =jdbcTemplate.queryForList("SELECT * from a");
+        System.out.println(list);
+
+    }
+
+}
