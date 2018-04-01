94fe2920ac7c682c4772c038a3d1edabdc8afd2a
15662650727 <447661866@qq.com>
2018-04-02 00:05:16 +0800

    1

--- src/com/ssm/controller/UserController.java
+++ src/com/ssm/controller/UserController.java
@@ -17,12 +17,12 @@ public class UserController {
 
     @RequestMapping("/{id}")
     @ResponseBody
-    public UserBean testValueC(@PathVariable int id){
+    public String testValueC(@PathVariable int id){
         System.out.println(id); //http://localhost:8080/a/b/123
         UserBean userBean = userService.getUserByID(id);
         System.out.println(userBean.toString());
 
-        return userBean;
+        return "成功";
     }
 
 }
