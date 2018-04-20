ce220b137c37a58256f189a4ce8b2853bfa44dec
15662650727 <447661866@qq.com>
2018-04-20 14:07:46 +0800

    12

---
+++ src/com/listener/listenerDemo.java
@@ -0,0 +1,22 @@
+package com.listener;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class listenerDemo implements ServletContextListener{
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println(1234);
+        System.out.println(1234);
+        //监听器.启动项目时候时候运行
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println(4567);
+        System.out.println(4567);
+        //监听器.结束项目时候运行
+    }
+}
--- src/com/springMvc/Hello.java
+++ src/com/springMvc/Hello.java
@@ -2,6 +2,7 @@ package com.springMvc;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,21 +11,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@Scope(value = "singleton")
 @RequestMapping("/a")
-public class Hello extends HelloF{
-    @Value("${testword}")  //需要在dispatcher-servlet.xml中的 <context:property-placeholder location="classpath:*.properties" /> 配置
+public class Hello extends HelloF {
+    @Value("${testword}")
+    //需要在dispatcher-servlet.xml中的 <context:property-placeholder location="classpath:*.properties" /> 配置
     private String string;
 
+    private int b = 0;
+    private static int c = 0;
+
+
     @RequestMapping("/b")
-    public void testValue(){
-        System.out.println(123); //http://localhost:8080/a/b
-        System.out.println(456);
+    public void testValue() {
+        System.out.println("12".hashCode()); //http://localhost:8080/a/b
         System.out.println(string);
+        System.out.println(b++);
+        int[] d = new int[1];
+        try {
+            System.out.println(d[2]);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     @RequestMapping("/b/{c}")
     @ResponseBody
-    public String testValueC(@PathVariable String c){
+    public String testValueC(@PathVariable String c) {
         System.out.println(123); //http://localhost:8080/a/b/123
         System.out.println(c);
         return c;
