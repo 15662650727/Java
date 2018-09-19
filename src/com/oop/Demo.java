b1aee15829ab08209352d0fc878c8107c6b04c47
15662650727 <447661866@qq.com>
2018-09-19 09:36:19 +0800

    9

---
+++ src/com/oop/Demo.java
@@ -0,0 +1,29 @@
+package com.oop;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author Zhang Yu
+ * @Date 2018-8-20 14:06
+ * @Version 1.0
+ **/
+public class Demo {
+
+    public static void main(String[] args) {
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Single single = new Single();
+        Single singl1e = new Single();
+        Single sing1le = new Single();
+        Single s1ingle = new Single();
+        Single sing1le1 = new Single();
+
+
+    }
+
+}
---
+++ src/com/oop/Single.java
@@ -0,0 +1,21 @@
+package com.oop;
+
+/**
+ * @ClassName Single
+ * @Description TODO
+ * @Author Zhang Yu
+ * @Date 2018-8-20 14:04
+ * @Version 1.0
+ **/
+public class Single {
+
+    public static GroudFather s = new GroudFather();
+
+    public Single(){
+
+    }
+
+
+
+
+}
